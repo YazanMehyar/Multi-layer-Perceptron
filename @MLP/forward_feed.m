71f7afa4b91c02b11fdcc5e022c01a04dfc6af4c
Yazan Mehyar <stcyazanerror@gmail.com>
2015-10-31 19:56:14 +0000

    Forward Feed basic algorithm

--- @MLP/forward_feed.m
+++ @MLP/forward_feed.m
@@ -1,15 +1,12 @@
 function result = forward_feed(this, sample)
   %first layer
-  for node = 1 : 10
-    this.InOutCell(1,node) = logsig(sum(this.weightsCell{1,node} .* [sample,1]));
-  end
+  this.layer_output{1} = logsig(sum(this.layer_w{1} .* repmat([sample,1],this.hidden_nodes,1), 2))';
 
-  % hidden layers and output
-  for layer = 2 : this.num_layers
-    for node = 1 : 10
-      this.InOutCell(layer,node) = logsig(sum(this.weightsCell{layer,node} .* [this.InOutCell(layer - 1,:),1]));
-    end
+  % hidden layers
+  for layer = 2 : this.num_layers - 1
+    this.layer_output{layer} = logsig(sum(this.layer_w{layer} .* repmat([this.layer_output{layer - 1},1],this.hidden_nodes,1), 2))';
   end
-
-  result = this.InOutCell(this.num_layers,:)
+  % output layer
+  this.layer_output{this.num_layers} = logsig(sum(this.layer_w{this.num_layers} .* repmat([this.layer_output{this.num_layers - 1},1],this.output_nodes,1), 2))';
+  result = this.layer_output{this.num_layers};
 end
